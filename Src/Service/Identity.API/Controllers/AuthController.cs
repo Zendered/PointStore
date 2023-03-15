d7b48cba0911e5adef40076e9f2960cbc9c1c310
zend <luizz.ggr@gmail.com>
2023-03-14 23:46:44 -0300

    generating jwt token

--- Src/Service/Identity.API/Controllers/AuthController.cs
+++ Src/Service/Identity.API/Controllers/AuthController.cs
@@ -1,6 +1,10 @@
 ﻿using Identity.API.Models.AccountViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace Identity.API.Controllers
 {
@@ -10,11 +14,16 @@ namespace Identity.API.Controllers
     {
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly UserManager<IdentityUser> _userManager;
+        private readonly IConfiguration _config;
 
-        public AuthController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        public AuthController(
+            SignInManager<IdentityUser> signInManager,
+            UserManager<IdentityUser> userManager,
+            IConfiguration config)
         {
             _signInManager = signInManager;
             _userManager = userManager;
+            _config = config;
         }
 
         [HttpPost("register")]
@@ -35,7 +44,7 @@ namespace Identity.API.Controllers
             if (result.Succeeded)
             {
                 await _signInManager.SignInAsync(user, isPersistent: false);
-                return Ok();
+                return Ok(await GenareteJwt(userRegister.Email));
             }
 
             return BadRequest();
@@ -53,7 +62,62 @@ namespace Identity.API.Controllers
                 true
                 );
 
-            return result.Succeeded ? Ok() : BadRequest();
+            return result.Succeeded ? Ok(await GenareteJwt(userLogin.Email)) : BadRequest();
         }
+
+        [HttpGet]
+        public async Task<UserLoginResponseViewModel> GenareteJwt(string email)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+            var claims = await _userManager.GetClaimsAsync(user);
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            claims.Add(new Claim(JwtRegisteredClaimNames.Sub, user.Id));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Email, user.Email));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Nbf, ToUnixEpochDate(DateTime.UtcNow).ToString()));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Iat, ToUnixEpochDate(
+                DateTime.UtcNow).ToString(), ClaimValueTypes.Integer64));
+
+            foreach (var userRole in userRoles)
+            {
+                claims.Add(new Claim("role", userRole));
+            }
+
+            var identityClaims = new ClaimsIdentity();
+            identityClaims.AddClaims(claims);
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_config["AppSettings:Secret"]);
+
+            var token = tokenHandler.CreateToken(new SecurityTokenDescriptor
+            {
+                Issuer = _config["AppSettings:Issuer"],
+                Audience = _config["AppSettings:ValidIn"],
+                Subject = identityClaims,
+                Expires = DateTime.UtcNow.AddHours(double.Parse(_config["AppSettings:ExpirationTime"])),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            });
+
+            var encodedToken = tokenHandler.WriteToken(token);
+
+            var response = new UserLoginResponseViewModel
+            {
+                AccessToken = encodedToken,
+                ExpiresIn = TimeSpan.FromHours(double.Parse(_config["AppSettings:ExpirationTime"])).TotalSeconds,
+                UserToken = new UserTokenViewModel
+                {
+                    Id = user.Id,
+                    Email = user.Email,
+                    Claims = claims.Select(c => new UserClaimViewModel { Type = c.Type, Value = c.Value })
+                }
+            };
+
+            return response;
+        }
+
+        private static long ToUnixEpochDate(DateTime date) =>
+            (long)Math.Round((date.ToUniversalTime() -
+                new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero)).TotalSeconds);
     }
 }
---
+++ Src/Service/Identity.API/Models/AccountViewModels/UserLoginResponseViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Identity.API.Models.AccountViewModels
+{
+    public class UserLoginResponseViewModel
+    {
+        public string AccessToken { get; set; }
+        public double ExpiresIn { get; set; }
+        public UserTokenViewModel UserToken { get; set; }
+    }
+}
